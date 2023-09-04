6aa4d7ce1f22f32f0cf14fdbb02b7038ace6e4c2
Suraj Kumar Suman <87268061+surajks02@users.noreply.github.com>
2023-09-04 21:56:02 +0530

    Update App.jsx

--- project-3/src/App.jsx
+++ project-3/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import StartGame from "./components/startgame";
+import StartGame from "./components/StartGame";
 import GamePlay from "./components/GamePlay";
 
 
